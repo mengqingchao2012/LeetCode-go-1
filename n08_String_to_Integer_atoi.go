70073c32d8281416afb16ca38848cd5db75571b3
YangKian <45479280+YangKian@users.noreply.github.com>
2019-09-27 16:45:21 +0800

    add 8, 18, 66

---
+++ n08_String_to_Integer_atoi.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func myAtoi(str string) int {
+	start, p, length := 0, 0, len(str)
+	var negative = false //标记符号位
+	words := []byte(str)
+
+	for p < length && words[p] == ' ' { //去掉先导空格
+		p++
+	}
+	if p == length { //如果全是空格，直接返回
+		return 0
+	}
+
+	if words[p] == '+' { //去掉符号位
+		p++
+	} else if words[p] == '-' {
+		negative = true
+		p++
+	}
+
+	for p < length && words[p] == '0' { //去掉先导0
+		p++
+	}
+	start = p
+
+	for p < length && words[p] >= '0' && words[p] <= '9' { //遍历计算数字的长度
+		p++
+	}
+	if p == start { //如果没有检测到任何数字，直接返回
+		return 0
+	}
+
+	if p - start > 10 { //如果数字的长度超过10，则肯定超过了int的取值范围，直接根据符号返回极值
+		if negative {
+			return math.MinInt32
+		} else {
+			return math.MaxInt32
+		}
+	}
+
+	var num int64 = 0 //用long类型来存储，防止超限
+	for i := start; i < p; i++ {
+		num = num * 10 + int64(words[i] - '0') // 注意一定要写 -"0"
+	}
+	if negative {
+		num = -num
+	}
+
+	switch { //根据符号位返回最后的结果
+	case num < math.MinInt32:
+		return math.MinInt32
+	case num > math.MaxInt32:
+		return math.MaxInt32
+	default:
+		return int(num)
+	}
+}
+
+func main() {
+	res := myAtoi("2147483648")
+	fmt.Println(res)
+}
---
+++ n18_4Sum.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"sort"
+)
+
+//Time：O(n^3)，Space：O(1)
+func fourSum(nums []int, target int) [][]int {
+	var res [][]int
+
+	size := len(nums)
+	if nums == nil || size < 4 {
+		return res
+	}
+	sort.Ints(nums)
+
+	for i := size - 1; i >= 3; i-- {
+		if 4 * nums[i] < target {
+			break
+		}
+		for j := i - 1; j >= 2; j-- {
+			if 3 * nums[j] < target - nums[i] {
+				break
+			}
+			newTarget := target - nums[i] - nums[j]
+			left, right := 0, j - 1
+			for left < right {
+				sum := nums[left] + nums[right]
+				switch {
+				case sum == newTarget:
+					res = append(res, []int{nums[left], nums[right], nums[j], nums[i]})
+					left, right = check(nums, left, right)
+				case sum < newTarget:
+					left++
+				case sum > newTarget:
+					right--
+				}
+			}
+			for j >= 2 && nums[j] == nums[j - 1] {
+				j--
+			}
+		}
+		for i >= 3 && nums[i] == nums[i - 1] {
+			i--
+		}
+	}
+	return res
+}
+
+func check(nums []int, left, right int) (int, int) {
+	for left < right {
+		switch {
+		case nums[left] == nums[left + 1]:
+			left++
+		case nums[right] == nums[right - 1]:
+			right--
+		default:
+			left++
+			right--
+			return left, right
+		}
+	}
+	return left, right
+}
+
+//func main() {
+//	res := fourSum([]int{0, 0, 0, 0}, 0)
+//	fmt.Println(res)
+//}
