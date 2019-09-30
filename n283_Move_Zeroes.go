6071b2fc2ef37a9ad81f1a7d2ae70f3ab9cc0e4c
YangKian <45479280+YangKian@users.noreply.github.com>
2019-09-30 22:43:42 +0800

    add 283

---
+++ n283_Move_Zeroes.go
@@ -0,0 +1,18 @@
+package main
+
+//Time：O(n)，Space：O(1)
+func moveZeroes(nums []int)  {
+	length := len(nums)
+
+	if length == 0 {
+		return
+	}
+
+	slow, fast := 0, 0
+	for ; fast < length; fast++ {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+	}
+}
