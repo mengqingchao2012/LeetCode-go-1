cba0da911c9baf72e0bdbff60543afff1d1c4cd6
YangKian <45479280+YangKian@users.noreply.github.com>
2019-10-03 23:23:18 +0800

    add 67

---
+++ n67_Add_Binary.go
@@ -0,0 +1,32 @@
+package main
+
+import "strconv"
+
+func addBinary(a string, b string) string {
+	la := len(a)
+	lb := len(b)
+	temp := make([]int, 0, max(la, lb) + 1)
+	var res string
+
+	i, j := la - 1, lb - 1
+	var carry = 0
+	for i >= 0 || j >= 0 || carry != 0 {
+		if i >= 0 {
+			carry += int(a[i] - '0')
+			i--
+		}
+		if j >= 0 {
+			carry += int(b[j] - '0')
+			j--
+		}
+		temp = append(temp, carry % 2)
+		carry /= 2
+	}
+
+	for i := len(temp) - 1; i >= 0; i-- {
+		res += strconv.Itoa(temp[i])
+	}
+
+	return res
+}
+
