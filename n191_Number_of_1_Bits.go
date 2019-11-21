89d3e221925afea7248f2c6d6e25a0a905f0f218
YangKian <45479280+YangKian@users.noreply.github.com>
2019-11-21 21:45:42 +0800

    add anotation for 191, add 509

--- n191_Number_of_1_Bits.go
+++ n191_Number_of_1_Bits.go
@@ -1,14 +1,16 @@
 package main
 
+//Time：O(k)，k是二进制中1的个数，Space：O(1)
 func hammingWeight(num uint32) int {
 	count := 0
 	for num != 0 {
 		count++
-		num &= (num - 1)
+		num &= (num - 1) // 将num的二进制中最低位的1消除
 	}
 	return count
 }
 
+//Time：O(n)，n是二进制的位数，Space：O(1)
 func hammingWeight1(num uint32) int {
 	var mask uint32 = 1
 	count := 0
---
+++ n509_Fibonacci_Number.go
@@ -0,0 +1,17 @@
+package main
+
+func fib(N int) int {
+	if N == 0 {
+		return 0
+	}
+
+	if N == 1 {
+		return 1
+	}
+
+	first, second := 0, 1
+	for i := 2; i <= N; i++ {
+		first, second = second, first + second
+	}
+	return second
+}
