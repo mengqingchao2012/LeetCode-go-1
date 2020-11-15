86a999390d9b1b7cda2ee41a8926b7a378116d6f
YangKian <45479280+YangKian@users.noreply.github.com>
2020-11-15 23:19:58 +0800

    update

--- sort/unstableSort/quickSort.go
+++ sort/unstableSort/quickSort.go
@@ -1,20 +1,20 @@
 package main
 
-//Time：O(nlogn)，Space：O(n)
-func lomutoPartition(nums *[]int, start, end int) int {
-	pivot := (*nums)[end]
+// lomuto 分割法，Time：O(nlogn)，Space：O(n)
+func lomutoPartition(nums []int, start, end int) int {
+	pivot := nums[end]
 	i := start
 	for j := start; j < end; j++ {
-		if (*nums)[j] < pivot {
-			(*nums)[i], (*nums)[j] = (*nums)[j], (*nums)[i]
+		if nums[j] < pivot {
+			nums[i], nums[j] = nums[j], nums[i]
 			i++
 		}
 	}
-	(*nums)[i], (*nums)[end] = (*nums)[end], (*nums)[i]
+	nums[i], nums[end] = nums[end], nums[i]
 	return i
 }
 
-func lomutoSort(nums *[]int, start, end int) {
+func lomutoSort(nums []int, start, end int) {
 	if start < end {
 		//注意 lomuto 分隔得到的分隔点 k 的位置就是其最终位置
 		k := lomutoPartition(nums, start, end)
@@ -27,7 +27,43 @@ func LomutoSort(nums []int) {
 	if len(nums) == 0 {
 		return
 	}
-	lomutoSort(&nums, 0, len(nums)-1)
+	lomutoSort(nums, 0, len(nums)-1)
+}
+
+
+// 霍尔分割法
+func quickSort(nums []int, low, high int) {
+	if low >= high {
+		return
+	}
+
+	pivot := nums[low + ((high - low) >> 1)]
+	i, j := low, high
+	for {
+		for nums[i] < pivot {
+			i++
+		}
+		for nums[j] > pivot {
+			j --
+		}
+		if i >= j {
+			break
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		i++; j--
+	}
+
+	quickSort(nums, low, j)
+	quickSort(nums, j + 1, high)
+}
+
+func hoareSort(nums []int) {
+	n := len(nums)
+	if n == 0 {
+		return
+	}
+
+	quickSort(nums, 0, n - 1)
 }
 
 //func main() {
